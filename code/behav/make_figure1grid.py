e0c61079d1d24545530113ca447cd995cd08d458
CarinaFo <carinaforster0611@gmail.com>
2023-04-28 14:58:22 +0200

    added a plot and stats function for figure 1

--- code/behav/make_figure1grid.py
+++ code/behav/make_figure1grid.py
@@ -5,19 +5,24 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
-import statsmodels.api as sm
 import numpy as np
 
 # Set up data paths
 behavpath = 'D:\\expecon_ms\\data\\behav'
 savepath = 'D:\\expecon_ms\\figs\\behavior'
-os.chdir(behavpath)
 
 # Load data
-data = pd.read_csv('clean_bb.csv')
+data = pd.read_csv(behavpath + '\\clean_bb.csv')
 
 # Add a 'congruency' column
-data['congruency'] = ((data.cue == 0.25) & (data.sayyes == 0)) | ((data.cue == 0.75) & (data.sayyes == 1))
+data['congruency'] = ((data.cue == 0.25) & (data.sayyes == 0)) | ...
+((data.cue == 0.75) & (data.sayyes == 1))
+
+# add lagged variables
+data['lagsayyes'] = data['sayyes'].shift(1)
+data['lagcue'] = data['cue'].shift(1)
+data['lagisyes'] = data['isyes'].shift(1)
+data['lagcorrect'] = data['correct'].shift(1)
 
 # Calculate hit rates by participant and cue condition
 signal = data[data.isyes == 1]
@@ -59,14 +64,22 @@ c_diff = np.array(criterion_low) - np.array(criterion_high)
 # Define colors for the plot
 blue = '#2a95ffff'
 pink = '#ff2a2aff'
-colors = [blue, pink]
-medcolor = ['black', 'black']
 
 # Define a function to plot the figure
 
-def plot_figure1_grid():
-        
-    fig = plt.figure(figsize=(8,10),tight_layout=True) # original working was 10,12
+
+def plot_figure1_grid(colors=[blue, pink], medcolor=['black', 'black']):
+
+    """Plot the figure 1 grid and the behavioral data
+    Parameters
+    ----------
+        colors : list of strings
+            The colors to use for the low and high cue condition
+            medcolor : list of strings
+            The colors to use for the median lines in the boxplots
+            """
+
+    fig = plt.figure(figsize=(8, 10), tight_layout=True)  # original working was 10,12
     gs = gridspec.GridSpec(6, 4)
 
     schem_01_ax = fig.add_subplot(gs[0:2, 0:])
@@ -77,17 +90,19 @@ def plot_figure1_grid():
     schem_02_ax.set_yticks([])
     schem_02_ax.set_xticks([])
 
-    hr_ax = fig.add_subplot(gs[4,0])
+    hr_ax = fig.add_subplot(gs[4, 0])
     # Plot individual data points 
-    for index in range(len(low_condition_hr)):
-        hr_ax.plot(1, low_condition_hr.iloc[index,1],
-                marker='', markersize=8, color=colors[0], markeredgecolor=colors[0], alpha=.5)
-        hr_ax.plot(2, high_condition_hr.iloc[index,1],
-                marker='', markersize=8, color=colors[1], markeredgecolor=colors[1], alpha=.5)
-        hr_ax.plot([1,2], [low_condition_hr.iloc[index,1], high_condition_hr.iloc[index,1]],
-                marker='', markersize=0, color='gray', alpha=.25)
+    for index in range(len(hitrate_low)):
+        hr_ax.plot(1, hitrate_low.iloc[index],
+                   marker='', markersize=8, color=colors[0], 
+                   markeredgecolor=colors[0], alpha=.5)
+        hr_ax.plot(2, hitrate_high.iloc[index],
+                   marker='', markersize=8, color=colors[1], 
+                   markeredgecolor=colors[1], alpha=.5)
+        hr_ax.plot([1, 2], [hitrate_low.iloc[index], hitrate_high.iloc[index]],
+                   marker='', markersize=0, color='gray', alpha=.25)
 
-    hr_box = hr_ax.boxplot([low_condition_hr.iloc[:,1], high_condition_hr.iloc[:,1]], patch_artist=True)
+    hr_box = hr_ax.boxplot([hitrate_low, hitrate_high], patch_artist=True)
 
     # Set the face color and alpha for the boxes in the plot
     for patch, color in zip(hr_box['boxes'], colors):
@@ -99,20 +114,21 @@ def plot_figure1_grid():
         patch.set_color(color)
         
     hr_ax.set_ylabel('hit rate', fontname="Arial", fontsize=14)
-    hr_ax.set_yticklabels(['0','0.5','1.0'], fontname="Arial", fontsize=12)
-    hr_ax.text(1.3,1,'***',verticalalignment='center',fontname='Arial',fontsize='18')
+    hr_ax.set_yticklabels(['0', '0.5', '1.0'], fontname="Arial", fontsize=12)
+    hr_ax.text(1.3, 1, '***', verticalalignment='center', fontname='Arial', fontsize='18')
 
+    fa_ax = fig.add_subplot(gs[5, 0])
+    for index in range(len(farate_high)):
+        fa_ax.plot(1, farate_low.iloc[index],
+                   marker='', markersize=8, color=colors[0], 
+                   markeredgecolor=colors[0], alpha=.5)
+        fa_ax.plot(2, farate_high.iloc[index],
+                   marker='', markersize=8, color=colors[1], 
+                   markeredgecolor=colors[1], alpha=.5)
+        fa_ax.plot([1, 2], [farate_low.iloc[index], farate_high.iloc[index]],
+                   marker='', markersize=0, color='gray', alpha=.25)
 
-    fa_ax = fig.add_subplot(gs[5,0])
-    for index in range(len(low_condition_fa)):
-        fa_ax.plot(1, low_condition_fa.iloc[index,1],
-                marker='', markersize=8, color=colors[0], markeredgecolor=colors[0], alpha=.5)
-        fa_ax.plot(2, high_condition_fa.iloc[index,1],
-                marker='', markersize=8, color=colors[1], markeredgecolor=colors[1], alpha=.5)
-        fa_ax.plot([1,2], [low_condition_fa.iloc[index,1], high_condition_fa.iloc[index,1]],
-                marker='', markersize=0, color='gray', alpha=.25)
-
-    fa_box = fa_ax.boxplot([low_condition_fa.iloc[:,1], high_condition_fa.iloc[:,1]], patch_artist=True)
+    fa_box = fa_ax.boxplot([farate_low, farate_high], patch_artist=True)
     # Set the face color and alpha for the boxes in the plot
     for patch, color in zip(fa_box['boxes'], colors):
         patch.set_facecolor(color)
@@ -123,29 +139,32 @@ def plot_figure1_grid():
         patch.set_color(color)
 
     fa_ax.set_ylabel('fa rate', fontname="Arial", fontsize=14)
-    fa_ax.set_yticklabels(['0','0.5','1.0'], fontname="Arial", fontsize=12)
-    fa_ax.text(1.3,1,'***',verticalalignment='center',fontname='Arial',fontsize='18')
+    fa_ax.set_yticklabels(['0', '0.5', '1.0'], fontname="Arial", fontsize=12)
+    fa_ax.text(1.3, 1, '***', verticalalignment='center', fontname='Arial', 
+               fontsize='18')
 
-
-    crit_ax = fig.add_subplot(gs[4:,1])
+    crit_ax = fig.add_subplot(gs[4:, 1])
     crit_ax .set_ylabel('c', fontname="Arial", fontsize=14)
-    crit_ax.text(1.3,1.5,'***',verticalalignment='center',fontname='Arial',fontsize='18')
+    crit_ax.text(1.3, 1.5, '***', verticalalignment='center', fontname='Arial',
+                 fontsize='18')
 
-    crit_ax.set_ylim(-0.5,1.5)
-    crit_ax.set_yticks([-0.5,0.5,1.5])
-    crit_ax.set_yticklabels(['-0.5','0.5','1.5'], fontname="Arial", fontsize=12)
+    crit_ax.set_ylim(-0.5, 1.5)
+    crit_ax.set_yticks([-0.5, 0.5, 1.5])
+    crit_ax.set_yticklabels(['-0.5', '0.5', '1.5'], fontname="Arial", 
+                            fontsize=12)
 
     for index in range(len(criterion_low)):
-        crit_ax.plot(1,criterion_low[index],
-            marker='o',markersize=0,color=colors[0],markeredgecolor=colors[0],alpha=.5)
-        crit_ax.plot(2,criterion_high[index],
-            marker='o',markersize=0,color=colors[1],markeredgecolor=colors[1],alpha=.5)
-        crit_ax.plot([1,2],[criterion_low[index],criterion_high[index]],
-            marker='',markersize=0,color='gray',alpha=.25)
+        crit_ax.plot(1, criterion_low[index],
+                     marker='o', markersize=0, color=colors[0], 
+                     markeredgecolor=colors[0], alpha=.5)
+        crit_ax.plot(2, criterion_high[index],
+                     marker='o', markersize=0, color=colors[1],
+                     markeredgecolor=colors[1], alpha=.5)
+        crit_ax.plot([1, 2], [criterion_low[index], criterion_high[index]],
+                     marker='', markersize=0, color='gray', alpha=.25)
         
     crit_box = crit_ax.boxplot([criterion_low, criterion_high],
-                        patch_artist=True)    
-        # Set the face color and alpha for the boxes in the plot
+                               patch_artist=True)  # Set the face color and alpha for the boxes in the plot
     for patch, color in zip(crit_box['boxes'], colors):
         patch.set_facecolor(color)
         patch.set_alpha(0.75)
@@ -154,25 +173,28 @@ def plot_figure1_grid():
     for patch, color in zip(crit_box['medians'], medcolor):
         patch.set_color(color)
 
-    dprime_ax = fig.add_subplot(gs[4:,2])
+    dprime_ax = fig.add_subplot(gs[4:, 2])
     dprime_ax .set_ylabel('dprime', fontname="Arial", fontsize=14)
-    dprime_ax.text(1.4,3,'n.s',verticalalignment='center',fontname='Arial',fontsize='13')
+    dprime_ax.text(1.4, 3, 'n.s', verticalalignment='center', fontname='Arial',
+                   fontsize='13')
 
-    dprime_ax.set_ylim(0,3)
-    dprime_ax.set_yticks([0,1.5,3])
-    dprime_ax.set_yticklabels(['0','1.5','3.0'], fontname="Arial", fontsize=12)
+    dprime_ax.set_ylim(0, 3)
+    dprime_ax.set_yticks([0, 1.5, 3])
+    dprime_ax.set_yticklabels(['0', '1.5', '3.0'], fontname="Arial", 
+                              fontsize=12)
 
     for index in range(len(d_prime_low)):
-        dprime_ax.plot(1,d_prime_low[index],
-            marker='o',markersize=0,color=colors[0],markeredgecolor=colors[0],alpha=.5)
-        dprime_ax.plot(2,d_prime_high[index],
-            marker='o',markersize=0,color=colors[1],markeredgecolor=colors[1],alpha=.5)
-        dprime_ax.plot([1,2],[d_prime_low[index],d_prime_high[index]],
-            marker='',markersize=0,color='gray',alpha=.25)
+        dprime_ax.plot(1, d_prime_low[index],
+                       marker='o',markersize=0, color=colors[0], 
+                       markeredgecolor=colors[0], alpha=.5)
+        dprime_ax.plot(2, d_prime_high[index],
+                       marker='o', markersize=0, color=colors[1], 
+                       markeredgecolor=colors[1], alpha=.5)
+        dprime_ax.plot([1, 2], [d_prime_low[index], d_prime_high[index]],
+                       marker='', markersize=0, color='gray', alpha=.25)
         
     dprime_box = dprime_ax.boxplot([d_prime_low, d_prime_high],
-                        patch_artist=True)    
-        # Set the face color and alpha for the boxes in the plot
+                                   patch_artist=True)  # Set the face color and alpha for the boxes in the plot
     for patch, color in zip(dprime_box['boxes'], colors):
         patch.set_facecolor(color)
         patch.set_alpha(0.75)
@@ -181,81 +203,97 @@ def plot_figure1_grid():
     for patch, color in zip(dprime_box['medians'], medcolor):
         patch.set_color(color)
 
-    conf_ax = fig.add_subplot(gs[4:,3])
+    conf_ax = fig.add_subplot(gs[4:, 3])
     conf_ax .set_ylabel('mean confidence', fontname="Arial", fontsize=14)
-    conf_ax.set_yticklabels(['0','0.5','1.0'], fontname="Arial", fontsize=12)
-    conf_ax.text(1.3,1,'***',verticalalignment='center',fontname='Arial',fontsize='18')
+    conf_ax.set_yticklabels(['0', '0.5', '1.0'], fontname="Arial", fontsize=12)
+    conf_ax.text(1.3, 1, '***', verticalalignment='center', fontname='Arial',
+                 fontsize='18')
 
     # Plot individual data points 
-    for index in range(len(low_condition_hr)):
-        conf_ax.plot(1, con_condition.iloc[index,1],
-                marker='', markersize=8, color=colors[0], markeredgecolor=colors[0], alpha=.5)
-        conf_ax.plot(2, incon_condition.iloc[index,1],
-                marker='', markersize=8, color=colors[1], markeredgecolor=colors[1], alpha=.5)
-        conf_ax.plot([1,2], [con_condition.iloc[index,1], incon_condition.iloc[index,1]],
-                marker='', markersize=0, color='gray', alpha=.25)
+    for index in range(len(hitrate_high)):
+        conf_ax.plot(1, con_condition.iloc[index, 1],
+                     marker='', markersize=8, color=colors[0],
+                     markeredgecolor=colors[0], alpha=.5)
+        conf_ax.plot(2, incon_condition.iloc[index, 1],
+                     marker='', markersize=8, color=colors[1],
+                     markeredgecolor=colors[1], alpha=.5)
+        conf_ax.plot([1, 2], [con_condition.iloc[index, 1], 
+                             incon_condition.iloc[index, 1]],
+                     marker='', markersize=0, color='gray', alpha=.25)
 
-    conf_box = conf_ax.boxplot([con_condition.iloc[:,1], incon_condition.iloc[:,1]], patch_artist=True)
+    conf_box = conf_ax.boxplot([con_condition.iloc[:, 1], incon_condition.iloc[:, 1]],
+                               patch_artist=True)
 
     # Set the face color and alpha for the boxes in the plot
-    colors = ['white','black']
+    colors = ['white', 'black']
     for patch, color in zip(conf_box['boxes'], colors):
         patch.set_facecolor(color)
         patch.set_alpha(0.75)
 
     # Set the color for the medians in the plot
-    medcolor = ['black','white']
+    medcolor = ['black', 'white']
     for patch, color in zip(conf_box['medians'], medcolor):
         patch.set_color(color)
 
-    for plots in [hr_ax,fa_ax,conf_ax]:
-        plots.set_ylim(0,1)
-        plots.set_yticks([0,0.5,1.0])
+    for plots in [hr_ax, fa_ax, conf_ax]:
+        plots.set_ylim(0, 1)
+        plots.set_yticks([0, 0.5, 1.0])
 
-    for plots in [hr_ax,fa_ax,crit_ax,dprime_ax,conf_ax]:
+    for plots in [hr_ax, fa_ax, crit_ax, dprime_ax, conf_ax]:
         plots.spines['top'].set_visible(False)
         plots.spines['right'].set_visible(False)
-        plots.set_xticks([1,2])
-        plots.set_xlim(0.5,2.5)
-        plots.set_xticklabels(['',''])
+        plots.set_xticks([1, 2])
+        plots.set_xlim(0.5, 2.5)
+        plots.set_xticklabels(['', ''])
         if plots != hr_ax:
-            plots.set_xticklabels(['0.25','0.75'], fontname="Arial", fontsize=12)
+            plots.set_xticklabels(['0.25', '0.75'], fontname="Arial",
+                                  fontsize=12)
             plots.set_xlabel('P (Stimulus)', fontname="Arial", fontsize=14)
         if plots == conf_ax:
-            plots.set_xticklabels(['congruent', 'incongruent'], fontname="Arial", fontsize=12,rotation=30)
+            plots.set_xticklabels(['congruent', 'incongruent'],
+                                  fontname="Arial", fontsize=12,
+                                  rotation=30)
             plots.set_xlabel('')
 
-    fig.savefig(savepath + "\\figure1.svg",dpi=300, bbox_inches='tight', format='svg')
+    fig.savefig(savepath + "\\figure1.svg",dpi=300, bbox_inches='tight',
+                format='svg')
     plt.show()
 
+
 def stats_figure1():
-
-    ## stats for Figure 1 (all significant except criterion)
-
+    """stats for Figure 1
+    Parameters  ---------- None.
+    Returns
+    -------
+    None.
+    """
     # non parametric
-    t,p = stats.wilcoxon(criterion_high, criterion_low)
+    t, p = stats.wilcoxon(criterion_high, criterion_low)
     print(f'c: {p}')
 
-    ## stats (all significant except dprime)
-
-    # non parametric
-    t,p = stats.wilcoxon(d_prime_high, d_prime_low)
+    t, p = stats.wilcoxon(d_prime_high, d_prime_low)
     print(f'dprime: {p}')
 
-    # non parametric
-    t,p = stats.wilcoxon(hitrate_high, hitrate_low)
+    t, p = stats.wilcoxon(hitrate_high, hitrate_low)
     print(f'hitrate: {p}')
-    # non parametric
-    t,p = stats.wilcoxon(farate_high, farate_low)
+
+    t, p = stats.wilcoxon(farate_high, farate_low)
     print(f'farate: {p}')
 
-    # non parametric
-    t,p = stats.wilcoxon(con_condition.iloc[:,1], incon_condition.iloc[:,1])
+    t, p = stats.wilcoxon(con_condition.iloc[:, 1], incon_condition.iloc[:, 1])
     print(f'confidence in correct trials only: {p}')
+
+# Figure 2 is not cleaned up yet
 
 
 def figure2():
-        
+    """Figure 2
+    Parameters  ---------- None.
+    Returns
+    -------
+    None
+    """
+    # load data
     # Correlate criterion with dprime
 
     os.chdir(savepath)
@@ -301,7 +339,6 @@ def figure2():
 
     # only for the high exp trials
 
-    """ # Create strip plot with 'tip' column
     sns.stripplot(x=list(range(1,40)), y=choice.prev_high)
 
     # Set axis labels and title
@@ -315,7 +352,6 @@ def figure2():
     # Show the plot
     plt.show()
 
-    # Create strip plot with 'tip' column
     sns.stripplot(x=list(range(1,40)), y=choice.prev_low)
     """
     """ # Set axis labels and title
@@ -328,7 +364,7 @@ def figure2():
     plt.xticks([])
 
     # Show the plot
-    plt.show() """
+    plt.show()
 
     # Define repeater and alternator as boolean mask
 
