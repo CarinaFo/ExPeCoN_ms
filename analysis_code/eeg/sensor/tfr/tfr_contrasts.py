d3b4f59d03516032aed01d66465b0bbe3f1fe8f0
CarinaFo <carinaforster0611@gmail.com>
2023-06-08 18:12:17 +0200

    added freq band analysis for alpha and beta pow

--- analysis_code/eeg/sensor/tfr/tfr_contrasts.py
+++ analysis_code/eeg/sensor/tfr/tfr_contrasts.py
@@ -41,15 +41,16 @@ IDlist = ('007', '008', '009', '010', '011', '012', '013', '014', '015', '016',
           '047', '048', '049')
 
 
-def calculate_power_per_trial(tmin=-0.5, tmax=0,
-                              zero_pad=1, reject_criteria=dict(eeg=200e-6),
-                              flat_criteria=dict(eeg=1e-6)):
+def calculate_power_per_trial(tmin=-1, tmax=0.5,
+                              zero_pad=0, reject_criteria=dict(eeg=200e-6),
+                              flat_criteria=dict(eeg=1e-6), 
+                              induced_power=0):
 
     '''calculate prestimulus time-frequency representations per trial
       (induced power) using multitaper method. Data is cut between tmin and
       tmax and zero padded on both ends to prevent edge artifacts and poststimulus
       leakage. Data is then saved in a tfr object per subject and stored to disk as
-        a .h5 file.
+      a .h5 file.
 
         Parameters
         ----------
@@ -67,6 +68,8 @@ def calculate_power_per_trial(tmin=-0.5, tmax=0,
     
     freq_list = np.arange(6, 35, 1)
     n_cycles = freq_list / 2.0  # different number of cycle per frequency
+
+    df_all = []
 
     for counter, subj in enumerate(IDlist):
 
@@ -101,8 +104,9 @@ def calculate_power_per_trial(tmin=-0.5, tmax=0,
         # remove first trial of each block (trigger delays)
         epochs = epochs[epochs.metadata.trial != 1]
 
-        # subtract evoked response
-        epochs = epochs.subtract_evoked()
+        if induced_power == 1:
+            # subtract evoked response
+            epochs = epochs.subtract_evoked()
 
         # load behavioral data
         data = pd.read_csv(f"{behavpath}//prepro_behav_data.csv")
@@ -120,8 +124,9 @@ def calculate_power_per_trial(tmin=-0.5, tmax=0,
         # drop bad epochs
         epochs.drop_bad(reject=reject_criteria, flat=flat_criteria)
 
-        # save behavioral data after bad epochs rejection
-        data = pd.read_csv(f"{behavpath}//prepro_behav_data_after_rejectepochs.csv")
+        metadata = epochs.metadata
+
+        df_all.append(metadata)
 
         # crop the data in the pre-stimulus window
         epochs.crop(tmin, tmax)
@@ -141,22 +146,35 @@ def calculate_power_per_trial(tmin=-0.5, tmax=0,
         power = mne.time_frequency.tfr_multitaper(epochs, n_cycles=n_cycles, freqs=freq_list, return_itc=False,
                                                   n_jobs=-1, average=False)
         # save the tfr object to disk
-        mne.time_frequency.write_tfrs(f"{tfr_single_trial_power_dir}//{subj}_power_per_trial-tfr.h5", power)
+        mne.time_frequency.write_tfrs(f"{tfr_single_trial_power_dir}//{subj}_power_per_trial_evoked_nozeropad-tfr.h5", power)
 
+    df = pd.concat(df_all)
+    df.to_csv(f"{behavpath}//prepro_behav_data_after_rejectepochs.csv")
 
-def contrast_conditions(cond='prevchoice'):
+def contrast_conditions(cond='highlow', cond_a='high',
+                        cond_b='low', induced_power=0,
+                        tmin=-1, tmax=0.5):
     
     '''calculate the difference between conditions for each subject
     average across trials and calculate grand average over participants
+    for each frequency and time point. Data is saved to disk as a .h5 file.
+    
     Parameters
     ----------
-    permute : int
-    if 1, permute conditions and calculate difference between conditions
+    cond : str
+        condition to be contrasted
+    cond_a : str
+    cond_b : str
+    induced_power : int
+        1 if induced power should be calculated, 0 if evoked power should be calculated
+    tmin : float
+    tmax : float
     Returns
-    list containing the difference between conditions for each subject
+    -------
+    None
     '''
 
-    diff_all_subs = []
+    evokeds_high, evokeds_low = [], []
 
     # load behavioral data 
     # (make sure has the same amount of trials as epochs)
@@ -165,23 +183,46 @@ def contrast_conditions(cond='prevchoice'):
 
     for counter, subj in enumerate(IDlist):
        
+        # save induced power
+        power_induced_all = []
+
         # skip those participants
         if subj == '040' or subj == '045' or subj == '032' or subj == '016':
             continue
 
-        power = mne.time_frequency.read_tfrs(f"{tfr_single_trial_power_dir}\\{subj}_power_per_trial-tfr.h5")[0]
+        power = mne.time_frequency.read_tfrs(f"{tfr_single_trial_power_dir}\\{subj}_power_per_trial_evoked_nozeropad-tfr.h5")[0]
         
         # add metadata
         subj_data = data[data.ID == counter+7]
 
         power.metadata = subj_data
 
-        if cond == 'highlow_signal':
+        power = power.copy().crop(tmin, tmax)
+
+        if induced_power == 1:
+
+            # subtract evoked response
+            power_evoked = power.average()
+
+            for epoch in range(power.data.shape[0]):
+                power_trial = power.data[epoch,:,:,:]
+                power_induced = power_trial - power_evoked.data
+                power_induced_all.append(power_induced)
+            
+            power_epochs = np.array(power_induced_all)
+
+            # put back into epochs structure
+
+            power = mne.time_frequency.EpochsTFR(data=power_epochs,
+                                                 info=power.info,
+                                                 times=power.times,
+                                                 freqs=power.freqs,
+                                                 metadata=power.metadata)
+
+        if cond == 'highlow':
             # get high and low probability trials 
-            power_a = power[((power.metadata.cue == 0.75)
-                             & (power.metadata.isyes == 1))]
-            power_b = power[((power.metadata.cue == 0.25)
-                             & (power.metadata.isyes == 1))]
+            power_a = power[((power.metadata.cue == 0.75))]
+            power_b = power[((power.metadata.cue == 0.25))]
         elif cond == 'prevchoice':
             # get previous no and previous yes trials
             power_a = power[(power.metadata.prevsayyes == 1)]
@@ -197,21 +238,21 @@ def contrast_conditions(cond='prevchoice'):
         evoked_power_a = power_a.average()
         evoked_power_b = power_b.average()
 
-        # calculate the difference between conditions
-        diff = evoked_power_a - evoked_power_b
-
-        # save the difference between conditions for each subject
-        diff_all_subs.append(diff)
+        evokeds_high.append(evoked_power_a)
+        evokeds_low.append(evoked_power_b)
 
     # Save the list to a file
-    with open(f'{tfr_contrast_dir}//diff_all_subs_{cond}.pickle', 'wb') as file:
-        pickle.dump(diff_all_subs, file)
+    with open(f'{tfr_contrast_dir}//{cond_a}_all_subs_evoked_nzp.pickle', 'wb') as file:
+        pickle.dump(evokeds_high, file)
 
-    return diff_all_subs
+    with open(f'{tfr_contrast_dir}//{cond_b}_all_subs_evoked_nzp.pickle', 'wb') as file:
+        pickle.dump(evokeds_low, file)
 
-def plot_grand_average(cond='hitmiss',
-                       tmin=-0.5, tmax=0,
-                       channel_names=['CP4', 'CP6', 'C4', 'C6']):
+    return evokeds_high, evokeds_low
+
+def plot_grand_average(cond_a='high', cond_b='low',
+                       tmin=-0.8, tmax=0.2,
+                       channel_names=['CP4', 'C4', 'C6', 'CP6']):
 
     '''plot the grand average of the difference between conditions
     after loading the contrast data using pickle
@@ -229,18 +270,90 @@ def plot_grand_average(cond='hitmiss',
 
     # Load the list from the file
     with open(f'{tfr_contrast_dir}\\'
-              f'diff_all_subs_{cond}.pickle', 'rb') as file:
-        contrast_data = pickle.load(file)
+              f'{cond_a}_all_subs_evoked_nzp.pickle', 'rb') as file:
+        evokeds_high = pickle.load(file)
 
-    diff_gra = mne.grand_average(contrast_data).apply_baseline(
-                                 baseline=(-1, -0.8))
+    with open(f'{tfr_contrast_dir}\\'
+              f'{cond_b}_all_subs_evoked_nzp.pickle', 'rb') as file:
+        evokeds_low = pickle.load(file)
+    
+    # baseline correction
+    evokeds_high = [e.apply_baseline((-1,-0.8), mode='zscore') for e in evokeds_high]
+    evokeds_low = [e.apply_baseline((-1,-0.8), mode='zscore') for e in evokeds_low]
 
-    diff_gra.copy().crop(tmin, tmax).plot_joint()
+    # contrast data
+    contrast_data = np.array([h.crop(tmin,tmax).pick_channels(channel_names).data-l.crop(tmin,tmax).pick_channels(['CP4']).data for h,l in zip (evokeds_high, evokeds_low)])
+    
+    high_data = np.array([h.crop(tmin,tmax).pick_channels(channel_names).data for h in evokeds_high])
+    low_data = np.array([l.crop(tmin,tmax).pick_channels(channel_names).data for l in evokeds_low])
 
-    topo = diff_gra.copy().crop(tmin, tmax).plot_topo()
+    # average over alpha band and participants
+    alpha_high = np.mean(high_data[:,:,2:8,:], axis=(0,1,2))
+    alpha_low = np.mean(low_data[:,:,2:8,:], axis=(0,1,2))
 
-    tfr = diff_gra.copy().crop(tmin, tmax).plot(
-          picks=channel_names, combine='mean')[0]
+    alpha_high_sub = np.mean(high_data[:,:,2:8,:], axis=(1,2))
+    alpha_low_sub = np.mean(low_data[:,:,2:8,:], axis=(1,2))
+
+    diff = alpha_high_sub-alpha_low_sub
+    
+    t,p,H = mne.stats.permutation_t_test(diff)
+
+    alpha_sd_high = np.std(high_data[:,:,2:8,:], axis=(0,1,2))
+    alpha_sd_low = np.std(low_data[:,:,2:8,:], axis=(0,1,2))
+
+    beta_high = np.mean(high_data[:,:,14:20,:], axis=(0,1,2))
+    beta_low = np.mean(low_data[:,:,14:20,:], axis=(0,1,2))
+
+    beta_high_sub = np.mean(high_data[:,:,14:20,:], axis=(1,2))
+    beta_low_sub = np.mean(low_data[:,:,14:20,:], axis=(1,2))
+
+    diff = beta_high_sub-beta_low_sub
+
+    t,p,H = mne.stats.permutation_t_test(diff)
+
+    beta_sd_high = np.std(high_data[:,:,2:8,:], axis=(0,1,2))
+    beta_sd_low = np.std(low_data[:,:,2:8,:], axis=(0,1,2))
+
+    plt.plot(np.linspace(tmin, tmax, alpha_high.shape[0]), alpha_high, label='alpha 0.75')
+    plt.plot(np.linspace(tmin, tmax, alpha_high.shape[0]), alpha_low, label='alpha 0.25')
+    plt.legend()
+    plt.xlabel('Time (s)')
+    plt.ylabel('Power')
+    plt.savefig(f'{savedir_figure4}\\{cond_a}_{cond_b}_alpha.svg', dpi=300)
+
+    plt.plot(np.linspace(tmin, tmax, alpha_high.shape[0]), beta_high, label='beta high')
+    plt.plot(np.linspace(tmin, tmax, alpha_high.shape[0]), beta_low, label='beta low')
+
+    plt.legend()
+    plt.xlabel('Time (s)')
+    plt.ylabel('Power')
+    plt.savefig(f'{savedir_figure4}\\{cond_a}_{cond_b}_beta_zoom.svg', dpi=300)
+
+
+    contrast_data = np.array([h.crop(tmin,tmax).data-l.crop(tmin,tmax).data for h,l in zip (evokeds_high, evokeds_low)])
+    
+    X = contrast_data.mean(axis=0)
+
+    X_tfr = mne.time_frequency.AverageTFR(data=X, 
+                                          times=evokeds_high[0].times,
+                                          freqs=evokeds_high[0].freqs, 
+                                          nave=39, 
+                                          info=evokeds_high[0].info)
+    X_tfr.crop(-0.5,0).plot(picks=['CP4'])
+
+    high_gra = mne.grand_average(evokeds_high)
+    low_gra = mne.grand_average(evokeds_low)
+
+    topo = high_gra.plot_topo()
+    topo = low_gra.plot_topo()
+
+    tfr = high_gra.plot(picks=channel_names, combine='mean')[0]
+    tfr = low_gra.plot(picks=channel_names, combine='mean')[0]
+
+    diff = high_gra - low_gra
+
+    diff.apply_baseline((-0.4,0), mode='logratio').copy().crop(-0.4,0).plot(picks=channel_names, 
+                                  combine='mean')[0]
 
     topo.savefig(f'{savedir_figure4}//{cond}_topo.svg')
     
@@ -262,24 +375,25 @@ def run_2D_cluster_perm_test(channel_names=['CP4', 'CP6', 'C4', 'C6'],
     None
     '''
 
+    contrast_data = np.array([h.crop(tmin,tmax).data-l.crop(tmin,tmax).data for h,l in zip (evokeds_high, evokeds_low)])
+
     spec_channel_list = []
 
     for i, channel in enumerate(channel_names):
-        spec_channel_list.append(contrast_data[15].ch_names.index(channel))
+        spec_channel_list.append(evokeds_high[15].ch_names.index(channel))
     spec_channel_list
 
-    X = np.array([d.crop(-0.5, 0).data for d in contrast_data])
+    mean_over_channels = np.mean(contrast_data[:, spec_channel_list, :, :], axis=1)
 
-    mean_over_channels = np.mean(X[:, spec_channel_list, :, :], axis=1)
-
-    threshold_tfce = dict(start=0, step=0.05)
+    threshold_tfce = dict(start=0, step=0.01)
 
     T_obs, clusters, cluster_p_values, H0 = mne.stats.permutation_cluster_1samp_test(
                             mean_over_channels,
                             n_jobs=-1,
                             n_permutations=n_perm,
                             threshold=threshold_tfce,
-                            tail=0)
+                            tail=0, 
+                            seed=1234)
     
     print(f'The minimum p-value is {min(cluster_p_values)}')
     
@@ -393,11 +507,9 @@ def run_cluster_correlation_2D(cond='lowhigh', channel_names=['CP4', 'CP6', 'C4'
         spec_channel_list.append(contrast_data[15].ch_names.index(channel))
     spec_channel_list
 
-    X = np.array([d.crop(-0.5, 0).data for d in contrast_data])
+    mean_over_channels = np.mean(contrast_data[:, spec_channel_list, :, :], axis=1)
 
-    mean_over_channels = np.mean(X[:, spec_channel_list, :, :], axis=1)
-
-    cluster_correlation.permutation_cluster_correlation_test(mean_over_channels, crit)
+    cluster_correlation.permutation_cluster_correlation_test(mean_over_channels, crit, threshold=0.05)
 
 ########################################################## Helper functions
 
