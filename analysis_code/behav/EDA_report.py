9bddc0446010082262842ca4a3e3ddf8d15e50d0
CarinaFo <carinaforster0611@gmail.com>
2023-09-18 19:04:28 +0200

    included expecon 2 data path

--- analysis_code/behav/EDA_report.py
+++ analysis_code/behav/EDA_report.py
@@ -2,16 +2,22 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-# save the preprocessing data
-behavpath = 'D:\\expecon_ms\\data\\behav\\behav_df\\'
+expecon = 2
+
+# which data set
+if expecon == 1:
+    behavpath = 'D:\\expecon_ms\\data\\behav\\behav_df\\'
+elif expecon == 2:
+    behavpath = 'D:\expecon_2\\behav\\'
 
 # Load the data
 df = pd.read_csv(f"{behavpath}prepro_behav_data.csv", na_values='?')
+
 df.drop(df.columns[0], axis=1, inplace=True)
 
 # Generate the report
 profile = ProfileReport(df,title="ExpecoN")
 
 # Save the report to .html
-profile.to_file(f"{behavpath}ExpecoN.html")
+profile.to_file(f"{behavpath}ExpecoN_str{str(expecon)}.html")
 
