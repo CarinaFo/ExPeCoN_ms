fbfc5ef7f539ecad07e1d9bc9864170a9cfe50e8
CarinaFo <carinaforster0611@gmail.com>
2023-09-18 19:36:24 +0200

    cleaned up

--- analysis_code/eeg/preprocessing/prepro.py
+++ analysis_code/eeg/preprocessing/prepro.py
@@ -1,24 +1,28 @@
-# file contains functions that does minimal preprocessing for raw eeg data (.fif files)
-# using the mne toolbox
+# file contains functions for preprocessing of EEG data using the mne toolbox
+# used for analysis of expecon study (ica is in a separate file caLLed ica.py)
 
-# please report bugs
-# Author: Carina Forster, forsteca@cbs.mpg.de
-
-# last update: 15.02.2023
+# author: Carina Forster
+# email: forsteca@cbs.mpg.de
 
 import copy
 import os
+import subprocess
 from pathlib import Path
 
 import mne
 import pandas as pd
-
-# for automatic detection of bad channels and bad epochs
 from autoreject import AutoReject, Ransac  # Jas et al., 2016
 
-# set directories
+# Specify the file path for which you want the last commit date
+file_path = "D:\expecon_ms\\analysis_code\\eeg\\preprocessing\\prepro.py"
 
+last_commit_date = subprocess.check_output(["git", "log", "-1", "--format=%cd", "--follow", file_path]).decode("utf-8").strip()
+print("Last Commit Date for", file_path, ":", last_commit_date)
+
+# set save paths
 save_dir_concatenated_raw = Path('D:/expecon_ms/data/eeg/raw_concatenated/')
+save_dir_stim = Path('D:/expecon_ms/data/eeg/prepro_stim/filter_0.1Hz/')
+save_dir_cue = Path('D:/expecon_ms/data/eeg/prepro_cue/')
 
 # EEG cap layout file
 filename_montage = Path('D:/expecon_ms/data/eeg/prepro_stim/CACS-64_REF.bvef')
@@ -26,31 +30,23 @@ filename_montage = Path('D:/expecon_ms/data/eeg/prepro_stim/CACS-64_REF.bvef')
 # raw behavioral data
 behavpath = Path('D:/expecon_ms/data/behav/behav_df/')
 
-# save cleaned EEG data
-save_dir_stim = Path('D:/expecon_ms/data/eeg/prepro_stim/filter_0.1Hz/')
-
-if not os.path.exists(save_dir_stim):
-    os.makedirs(save_dir_stim)
-    print("Directory created:", save_dir_stim)
-
-save_dir_cue = Path('D:/expecon_ms/data/eeg/prepro_cue/')
-
+# participant IDs
 IDlist = ['007', '008', '009', '010', '011', '012', '013', '014', '015', '016',
-          '017','018', '019', '020', '021', '022', '023', '024', '025', '026',
+          '017', '018', '019', '020', '021', '022', '023', '024', '025', '026',
           '027', '028', '029', '030', '031', '032', '033', '034', '035', '036',
-          '037', '038', '039', '040','041', '042', '043', '044', '045', '046',
+          '037', '038', '039', '040', '041', '042', '043', '044', '045', '046',
           '047', '048', '049']
 
 
 def concatenate():
 
-    """this function takes as input raw EEG data (brainvision files), 
+    """this function takes as input raw EEG data (brainvision files),
     reads the event triggers from annotations
-    and concatenates the raw data files to one big .fif file"""
+    and concatenates the raw data files for each experimental block to one big .fif file"""
 
     trigger_per_block = []
 
-    for index, subj in enumerate(IDlist):
+    for subj in enumerate(IDlist):
 
         # if file exists, skip
         if os.path.exists(f'{save_dir_concatenated_raw}\P{subj}_concatenated_raw.fif'):
@@ -85,10 +81,10 @@ def concatenate():
                 raw_3 = mne.io.read_raw_brainvision(raw_fname3)
                 raw_4 = mne.io.read_raw_brainvision(raw_fname4)
 
-                events_1, event_dict = mne.events_from_annotations(raw_1, regexp='Stimulus/S  2')
-                events_2, event_dict = mne.events_from_annotations(raw_2, regexp='Stimulus/S  2')
-                events_3, event_dict = mne.events_from_annotations(raw_3, regexp='Stimulus/S  2')
-                events_4, event_dict = mne.events_from_annotations(raw_4, regexp='Stimulus/S  2')
+                events_1, _ = mne.events_from_annotations(raw_1, regexp='Stimulus/S  2')
+                events_2, _ = mne.events_from_annotations(raw_2, regexp='Stimulus/S  2')
+                events_3, _ = mne.events_from_annotations(raw_3, regexp='Stimulus/S  2')
+                events_4, _ = mne.events_from_annotations(raw_4, regexp='Stimulus/S  2')
 
                 trigger_per_block.append([len(events_1), len(events_2), len(events_3), len(events_4),
                   len(events_5)])
@@ -103,15 +99,15 @@ def concatenate():
                 raw_4 = mne.io.read_raw_brainvision(raw_fname4)
                 raw_5 = mne.io.read_raw_brainvision(raw_fname5)
 
-                events_1, event_dict = mne.events_from_annotations(raw_1,
+                events_1, _ = mne.events_from_annotations(raw_1,
                                                                    regexp='Stimulus/S  2')
-                events_2, event_dict = mne.events_from_annotations(raw_2,
+                events_2, _ = mne.events_from_annotations(raw_2,
                                                                    regexp='Stimulus/S  2')
-                events_3, event_dict = mne.events_from_annotations(raw_3,
+                events_3, _ = mne.events_from_annotations(raw_3,
                                                                    regexp='Stimulus/S  2')
-                events_4, event_dict = mne.events_from_annotations(raw_4,
+                events_4, _ = mne.events_from_annotations(raw_4,
                                                                    regexp='Stimulus/S  2')
-                events_5, event_dict = mne.events_from_annotations(raw_5,
+                events_5, _ = mne.events_from_annotations(raw_5,
                                                                    regexp='Stimulus/S  2')
 
                 # check if we have 144 trigger per block (trials)
@@ -126,6 +122,7 @@ def concatenate():
             raw.save(f'{save_dir_concatenated_raw}P{subj}_concatenated_raw.fif')
     
     return trigger_per_block
+
 
 def remove_trials(filename=Path('/raw_behav_data.csv')):  
 
@@ -169,8 +166,9 @@ def remove_trials(filename=Path('/raw_behav_data.csv')):
 
     df.to_csv(f'{behavpath}{Path("/behav_cleaned_for_eeg.csv")}')
 
-def prepro(trigger=0, l_freq=0.1, h_freq=40,
-           tmin=-1.5, tmax=1.5, resample_rate=250, laplace=0, sf=2500,
+
+def prepro(trigger='stimulus', l_freq=0.1, h_freq=40,
+           tmin=-1.5, tmax=1.5, resample_rate=250, sf=2500,
            detrend=1, ransac=1, autoreject=0):
 
     """
@@ -190,18 +188,19 @@ def prepro(trigger=0, l_freq=0.1, h_freq=40,
       might create trigger jitter)
       """
 
-    # load the cleaned behavioral data for EEG preprocessing
+    # load the cleaned behavioral data for EEG preprocessing (kicked out trials with 
+    # no matching trigger in the EEG recording)
     df = pd.read_csv(f'{behavpath}{Path("/behav_cleaned_for_eeg.csv")}')
     
+    # set eeg channel layout for topo plots
     montage = mne.channels.read_custom_montage(filename_montage)
 
     # store how many channels were interpolated per participant
-    # and EEG annotations
+    # and annotations (trigger) information
 
     ch_interp, anot = [], []
 
     # loop over participants
-
     for index, subj in enumerate(IDlist):
 
         # if file exists, skip
@@ -210,7 +209,8 @@ def prepro(trigger=0, l_freq=0.1, h_freq=40,
             continue
         
         # load raw data concatenated for all blocks
-        raw = mne.io.read_raw_fif(f'{save_dir_concatenated_raw}{Path("/")}P{subj}_concatenated_raw.fif',
+        raw = mne.io.read_raw_fif(f'{save_dir_concatenated_raw}{Path("/")}'
+                                  f'P{subj}_concatenated_raw.fif',
                                   preload=True)
         
         # save the annotations (trigger) information
@@ -219,49 +219,43 @@ def prepro(trigger=0, l_freq=0.1, h_freq=40,
         raw.set_channel_types({'VEOG': 'eog', 'ECG': 'ecg'})
 
         # setting montage from brainvision montage file
-
         raw.set_montage(montage)
 
-        # filter the data
+        # bandpass filter the data
         raw.filter(l_freq=l_freq, h_freq=h_freq, method='iir')
 
         # load stimulus trigger events
-        events, event_dict = mne.events_from_annotations(raw, regexp='Stimulus/S  2')
+        events, _ = mne.events_from_annotations(raw, regexp='Stimulus/S  2')
 
-        # add cue as a trigger to event structure
-
-        if trigger == 1:
+        # add stimulus onset cue as a trigger to event structure
+        if trigger == "cue":
 
             cue_timings = [i - int(0.4 * sf) for i in
-                        events[:,
-                        0]]
+                        events[:, 0]]
 
             # subtract 0.4*sampling frequency to get the cue time stamps
-
             cue_events = copy.deepcopy(events)
             cue_events[:, 0] = cue_timings
             cue_events[:, 2, ] = 2
 
         # add dataframe as metadata to epochs
-
-        df_sub = df[df.ID == index + 7]
+        df_sub = df[df.ID == index + 7] # first 6 subjects were pilot subjects
 
         metadata = df_sub
 
         # lock the data to the specified trigger and create epochs
-        if trigger == 1:
-
+        if trigger == "cue":
+            # stimulus onset cue
             epochs = mne.Epochs(raw, cue_events, event_id=2, tmin=tmin,
                                 tmax=tmax, preload=True, baseline=None, 
                                 detrend=detrend, metadata=metadata)
-        # or stimulus onset
+        # or stimulus
         else:
             epochs = mne.Epochs(raw, events, event_id=1, tmin=tmin, tmax=tmax,
                                 preload=True, baseline=None, detrend=detrend,
                                 metadata=metadata)
             
-        # resample the data (resampling before epoching jitters the trigger)
-
+        # downsample the data (resampling before epoching jitters the trigger)
         epochs.resample(resample_rate)
 
         # pick only EEG channels for Ransac bad channel detection 
@@ -271,7 +265,6 @@ def prepro(trigger=0, l_freq=0.1, h_freq=40,
         # (autoreject interpolates bad channels and detects bad
         # epochs, takes quite long and removes a lot of epochs due to
         # blink artifacts)
-
         if ransac:
             
             print(f'Run ransac for {subj}')
@@ -287,9 +280,7 @@ def prepro(trigger=0, l_freq=0.1, h_freq=40,
             ch_interp.append(ransac.bad_chs_)
 
         # detect bad epochs
-
         # now feed the clean channels into Autoreject to detect bad trials
-
         if autoreject:
 
             ar = AutoReject()
@@ -298,16 +289,15 @@ def prepro(trigger=0, l_freq=0.1, h_freq=40,
 
             reject_log.save(f'{save_dir_stim}{Path("/")}P_{subj}_reject_log.npz')
 
-        if trigger == 1:
-
+        if trigger == "cue":
             epochs.save(f'{save_dir_cue}{Path("/")}P{subj}_epochs_cue-epo.fif')
 
         else:
-
-            epochs.save(f'{save_dir_stim}{Path("/")}P{subj}_epochs_stim_0.1Hzfilter-epo.fif')
+            epochs.save(f'{save_dir_stim}{Path("/")}P{subj}_epochs_stim_{l_freq}Hz_filter-epo.fif')
 
             print(f'saved epochs for participant {subj}')
 
+    # for methods part: how many channels were interpolated per participant
     ch_df = pd.DataFrame(ch_interp)
 
     ch_df.to_csv(f'{save_dir_stim}{Path("/")}interpolated_channels.csv')
@@ -316,15 +306,13 @@ def prepro(trigger=0, l_freq=0.1, h_freq=40,
 
     return ch_interp, anot
 
-def channels_interp(df=None):
+
+def channels_interp():
     """
-    This function calculates the amount of channels interpolated per participant
-    and the mean, std, min and max of channels interpolated across participants
-    
+    This function calculates the mean, std, min and max of channels 
+    interpolated across participants
     Parameters
     ----------
-    df : pandas dataframe
-        dataframe containing the amount of channels interpolated per participant
     Returns
     -------
     None.
@@ -340,17 +328,19 @@ def channels_interp(df=None):
     print(f"min channels interpolated: {df['count_ch'].min()}")
     print(f"max channels interpolated: {df['count_ch'].max()}")
 
+
+# Unused functions
 def add_reaction_time_trigger():
+    """
+    this function adds the reaction time as a trigger to the event structure"""
 
-    # add reaction time as trigger event
-
-    # we reset the index because of the individual amount of trials removed in the
-    # beginning
-
+    # reset index
     metadata_index = metadata.reset_index()
 
+    # get rt per trial
     rt = metadata_index.respt1
 
+    # add event trigger
     rt_timings = [event + int(rt[index] * sf) for index, event in
                  enumerate(events[:, 0])]
 
