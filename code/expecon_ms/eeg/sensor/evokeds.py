66db0a0f083fe9da7c00cb6df769a5c24f50ce5a
CarinaFo <carinaforster0611@gmail.com>
2023-10-30 17:24:59 +0100

    implemented ToDos, added prepro config parameters

--- code/expecon_ms/eeg/sensor/evokeds.py
+++ code/expecon_ms/eeg/sensor/evokeds.py
@@ -21,7 +21,6 @@ import pandas as pd
 
 from expecon_ms.behav import figure1
 from expecon_ms.configs import PROJECT_ROOT, config, params, path_to
-
 # %% Set global vars & paths >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 # Specify the file path for which you want the last commit date
@@ -42,23 +41,31 @@ plt.rcParams.update({"font.size": 14, "font.family": "sans-serif", "font.sans-se
 
 # directory that contains the cleaned epochs
 dir_clean_epochs = Path(path_to.data.eeg.preprocessed.ica.ICA)
+dir_clean_epochs_expecon2 = Path(path_to.data.eeg.preprocessed.ica.clean_epochs_expecon2)
 
 # behavioral data for each epoch
 behav_path = Path(path_to.data.behavior)
 
 # participant IDs
-id_list = config.participants.ID_list
+id_list = config.participants.ID_list_expecon1
+id_list_expecon2 = config.participants.ID_list_expecon2
 
 # pilot data counter
 pilot_counter = config.participants.pilot_counter
 
-# blocks to delete
-ids_to_delete = config.behavioral_cleaning.ids_to_delete
-blocks_to_delete = config.behavioral_cleaning.blocks_to_delete
+# data_cleaning parameters defined in config.toml
+rt_max = config.behavioral_cleaning.rt_max
+rt_min = config.behavioral_cleaning.rt_min
+hitrate_max = config.behavioral_cleaning.hitrate_max
+hitrate_min = config.behavioral_cleaning.hitrate_min
+farate_max = config.behavioral_cleaning.farate_max
+hit_fa_diff = config.behavioral_cleaning.hit_fa_diff
 # %% Functions >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 
-def create_contrast(drop_bads: bool = True, laplace: bool = False, 
+def create_contrast(study: int = 1,
+                    drop_bads: bool = True,
+                    laplace: bool = False,
                     subtract_evoked: bool = False,
                     save_data_to_disk: bool = False):
     """
@@ -66,55 +73,69 @@ def create_contrast(drop_bads: bool = True, laplace: bool = False,
 
     Args:
     ----
+    study: int, study number (1 or 2), 1 == expecon1, 2 == expecon2
     drop_bads: boolean, drop bad epochs if True
     laplace: apply CSD to data if boolean is True
     subtract_evoked: boolean, subtract evoked signal from each epoch
     save_data_to_disk: boolean, save data to disk if True
-
     Returns:
     -------
     list of condition evokeds
     """
+
     # store trials remaining and removed per participant
     all_trials, trials_removed = [], []
 
     # save  evoked data per participant
-    evokeds_signal_all, evokeds_noise_all, evokeds_hit_all, evokeds_miss_all = [], [], [], []
+    evokeds_signal_all, evokeds_noise_all = [], []
+    evokeds_hit_all, evokeds_miss_all = [], []
 
     # metadata after epoch cleaning
     metadata_allsubs = []
 
+    # load behavioral dataframe
+    if study == 1:
+        data = pd.read_csv(f'{behav_path}{Path("/")}prepro_behav_data.csv')
+    else:
+        id_list = id_list_expecon2
+        data = pd.read_csv(f'{behav_path}{Path("/")}prepro_behav_data_expecon2.csv')
+
     for idx, subj in enumerate(id_list):
+
         # print participant idx
         print(f"Participant {idx!s}")
+        
+        if study == 1:
+            # load cleaned epochs
+            epochs = mne.read_epochs(dir_clean_epochs 
+                                     / f"P{subj}_icacorr_0.1Hz-epo.fif")
+        else:
+            # skip ID 13
+            if subj == '013':
+                continue
+            epochs = mne.read_epochs(dir_clean_epochs_expecon2 
+                                     / f"P{subj}_icacorr_0.1Hz-epo.fif")
+            epochs.metadata = epochs.metadata.rename(columns ={"resp1_t": "respt1",
+                                                               "stim_type": "isyes",
+                                                               "resp1": "sayyes"})
+            
+        # save for descriptives
+        before_cleaning = len(epochs.metadata)
 
-        # load cleaned epochs
-        epochs = mne.read_epochs(dir_clean_epochs / f"P{subj}_icacorr_0.1Hz-epo.fif")
-
-        # Check if the participant ID is in the list of IDs to delete
-        if pd.unique(epochs.metadata.ID) in ids_to_delete:
-            # Get the corresponding blocks to delete for the current participant
-            participant_blocks_to_delete = [
-                block for id_, block in zip(ids_to_delete, blocks_to_delete) if id_ == pd.unique(epochs.metadata.ID)
-            ]
-
-            # Drop the rows with the specified blocks from the dataframe
-            epochs = epochs[~epochs.metadata.block.isin(participant_blocks_to_delete)]
-
-        # remove trials with rts >= 2.5 (no response trials) and trials with rts < 0.1
-        before_rt_removal = len(epochs.metadata)
-        epochs = epochs[epochs.metadata.respt1 >= 0.1]
-        epochs = epochs[epochs.metadata.respt1 != 2.5]
+        # clean epochs (remove blocks based on hit and false alarm rates, reaction times, etc.)
+        epochs = drop_trials(data=epochs)
 
         # save n_trials per participant
         all_trials.append(len(epochs.metadata))
 
-        trials_removed.append(before_rt_removal - len(epochs.metadata))
+        # save number of trials removed per participant
+        trials_removed.append(before_cleaning - len(epochs.metadata))
 
-        # load behavioral data
-        data = pd.read_csv(f'{behav_path}{Path("/")}prepro_behav_data.csv')
-
-        subj_data = data[idx + pilot_counter == data.ID]
+        # get behavioral data for current participant
+        if study == 1:
+            subj_data = data[idx + pilot_counter == data.ID]
+        else:
+            subj_data = data[idx + 1 == data.ID]
 
         # get drop log from epochs
         drop_log = epochs.drop_log
@@ -162,22 +183,24 @@ def create_contrast(drop_bads: bool = True, laplace: bool = False,
         evokeds_noise_all.append(epochs_noise.average())
 
         if save_data_to_disk:
-
             with Path(path_to.data.eeg.sensor.erp.hits).open("wb") as fp:  # pickling
                 pickle.dump(evokeds_hit_all, fp)  
-
             with Path(path_to.data.eeg.sensor.erp.misses).open("wb") as fp:  # pickling
                 pickle.dump(evokeds_miss_all, fp)
 
         # save trial number and trials removed to csv file
-        pd.DataFrame(trials_removed).to_csv(f'{dir_clean_epochs}{Path("/")}trials_removed.csv')
-        pd.DataFrame(all_trials).to_csv(f'{dir_clean_epochs}{Path("/")}trials_per_subject.csv')
+        pd.DataFrame(trials_removed).to_csv(f'{dir_clean_epochs}{Path("/")}trials_removed_{str(study)}.csv')
+        pd.DataFrame(all_trials).to_csv(f'{dir_clean_epochs}{Path("/")}trials_per_subject_{str(study)}.csv')
 
     return [evokeds_signal_all, evokeds_noise_all, evokeds_hit_all, evokeds_miss_all]
 
 
-def plot_ssroi(tmin: float = -0.2, tmax: float = 0.5,
-               tmin_base: float = -0.1, tmax_base: float = 0):
+def plot_roi(data = None,
+             tmin: float = -0.2, 
+             tmax: float = 0.5,
+             tmin_base: float = -0.1,
+             tmax_base: float = 0.,
+             study: int = 2):
     """
     Plot topography of P50 for the contrast of signal - noise trials 
     and the contrast over time for the channel with the strongest effect.
@@ -192,12 +215,11 @@ def plot_ssroi(tmin: float = -0.2, tmax: float = 0.5,
     -------
     None
     """
-
-    conds = create_contrast()
+    data = create_contrast(study=2)
 
     # baseline correct and crop the data for each participant
-    signal = [s.apply_baseline((tmin_base, tmax_base)).crop(tmin, tmax) for s in conds[0]]
-    noise = [n.apply_baseline((tmin_base, tmax_base)).crop(tmin, tmax) for n in conds[1]]
+    signal = [s.copy().apply_baseline((tmin_base, tmax_base)).crop(tmin, tmax) for s in data[0]]
+    noise = [n.copy().apply_baseline((tmin_base, tmax_base)).crop(tmin, tmax) for n in data[1]]
 
     # combine evokeds (subtract noise from signal)
     x = [mne.combine_evoked([s, n], weights=[1, -1]) for s, n in zip(signal, noise)]
@@ -207,18 +229,21 @@ def plot_ssroi(tmin: float = -0.2, tmax: float = 0.5,
 
     # Figure 3: Sensory region of interest definition
     # Create a 1x2 grid of subplots
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+    _ , axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
     
     # plot topoplot for contrast around P50
-    x_gra.plot_topomap(times=[.05], average=[0.02], axes=axs[0], show=False, colorbar=False,
-                          show_names=False)
+    x_gra.plot_topomap(times=[.05], average=[0.02], axes=axs[0],
+                       show=False, colorbar=False,
+                       show_names=False)
 
     # plot contrast over time for CP4 (strongest effect)
-    x_gra.plot(axes=axs[1], show=False, picks=['CP4'])
+    x_gra.plot(axes=axs[1], show=False, picks=['C4'])
 
     # save image
     plt.tight_layout()
-    plt.savefig(Path(path_to.figures.manuscript.figure3, "fig3_ssroi_expecon1.svg"), dpi=300)
+    plt.savefig(Path(path_to.figures.manuscript.figure3,
+                     f"fig3_ssroi_{str(study)}.svg"),
+                    dpi=300)
     plt.show()
 
 
@@ -349,6 +374,84 @@ def plot_cluster_output_2D(tmin=0, tmax=0.3,
         plt.show()
 
 
+#### Helper functions
+
+
+def drop_trials(data=None):
+    """
+    Drop trials based on behavioral data.
+
+    Args:
+    ----
+    data: mne.Epochs, epoched data
+    
+    Returns:
+    -------
+    data: mne.Epochs, epoched data
+    """
+
+    # store number of trials before rt cleaning
+    before_rt_cleaning = len(data.metadata)
+
+    # remove no response trials or super fast responses
+    data = data[data.metadata.respt1 != rt_max]
+    data = data[data.metadata.respt1 > rt_min]
+
+    # print rt trials dropped
+    rt_trials_removed = before_rt_cleaning - len(data.metadata)
+
+    print("Removed trials based on reaction time: ", rt_trials_removed)
+    # Calculate hit rates per participant
+    signal = data[data.metadata.isyes == 1]
+    hitrate_per_subject = signal.metadata.groupby(['ID'])['sayyes'].mean()
+
+    print(f"Mean hit rate: {np.mean(hitrate_per_subject):.2f}")
+
+    # Calculate hit rates by participant and block
+    hitrate_per_block = signal.metadata.groupby(['ID', 'block'])['sayyes'].mean()
+
+    # remove blocks with hitrates > 90 % or < 20 %
+    filtered_groups = hitrate_per_block[(hitrate_per_block > hitrate_max) | (hitrate_per_block < hitrate_min)]
+    print('Blocks with hit rates > 0.9 or < 0.2: ', len(filtered_groups))
+
+    # Extract the ID and block information from the filtered groups
+    remove_hitrates = filtered_groups.reset_index()
+
+    # Calculate false alarm rates by participant and block
+    noise = data[data.metadata.isyes == 0]
+    farate_per_block = noise.metadata.groupby(['ID', 'block'])['sayyes'].mean()
+
+    # remove blocks with false alarm rates > 0.4
+    filtered_groups = farate_per_block[farate_per_block > farate_max]
+    print('Blocks with false alarm rates > 0.4: ', len(filtered_groups))
+
+    # Extract the ID and block information from the filtered groups
+    remove_farates = filtered_groups.reset_index()
+
+    # Hitrate should be > false alarm rate
+    filtered_groups = hitrate_per_block[hitrate_per_block - farate_per_block < hit_fa_diff]
+    print('Blocks with hit rates < false alarm rates: ', len(filtered_groups))
+          
+    # Extract the ID and block information from the filtered groups
+    hitvsfarate = filtered_groups.reset_index()
+
+    # Concatenate the dataframes
+    combined_df = pd.concat([remove_hitrates, remove_farates, hitvsfarate])
+
+    # Remove duplicate rows based on 'ID' and 'block' columns
+    unique_df = combined_df.drop_duplicates(subset=['ID', 'block'])
+
+    # Merge the big dataframe with unique_df to retain only the non-matching rows
+    data.metadata = data.metadata.merge(unique_df, on=['ID', 'block'], how='left',
+                             indicator=True,
+                             suffixes=('', '_y'))
+    
+    data = data[data.metadata['_merge'] == 'left_only']
+
+    # Remove the '_merge' column
+    data.metadata = data.metadata.drop('_merge', axis=1)
+
+    return data
 # %% __main__  >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 if __name__ == "__main__":
