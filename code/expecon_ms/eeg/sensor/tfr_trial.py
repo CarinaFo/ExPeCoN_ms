7c9d4cba0eb453b8b849a2e4714c2f9b15c8eb62
CarinaFo <carinaforster0611@gmail.com>
2023-10-31 16:40:46 +0100

    adjusted script for study 2/ToDos

--- code/expecon_ms/eeg/sensor/tfr_trial.py
+++ code/expecon_ms/eeg/sensor/tfr_trial.py
@@ -32,14 +32,16 @@ last_commit_date = (
 )
 print("Last Commit Date for", __file__path, ":", last_commit_date)
 
-# set font to Arial and font size to 22
+# set font to Arial and font size to 14
 plt.rcParams.update({"font.size": 14, "font.family": "sans-serif", "font.sans-serif": "Arial"})
 
 # set directory paths
-behav_dir = Path(path_to.data.behavior.behavior_df)
+behav_dir = Path(path_to.data.behavior)
 tfr_dir = Path(path_to.data.eeg.sensor.tfr)
 
-id_list = config.participants.ID_list
+# participant IDs
+id_list_expecon1 = config.participants.ID_list_expecon1
+id_list_expecon2 = config.participants.ID_list_expecon2
 
 # frequencies from tfr calculation
 freq_list = np.arange(3, 35, 1)
@@ -47,12 +49,12 @@ freq_list = np.arange(3, 35, 1)
 # define frequency bands we are interested in
 freq_bands = {"alpha": (7, 13), "beta": (15, 25)}
 
-
 # %% Functions >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 
 # TODO(simon): defaults should not be mutable
-def save_band_power_per_trial(time_intervals={"pre": (-0.2, 0)}, channel_name=["CP4"]):
+def save_band_power_per_trial(time_intervals={"pre": (-0.2, 0)}, channel_name=["CP4"],
+                              study: int = 2):
     """
     Save the power per trial per frequency band in a csv file.
 
@@ -66,7 +68,7 @@ def save_band_power_per_trial(time_intervals={"pre": (-0.2, 0)}, channel_name=["
     ----
     time_intervals: dict: Dictionary with time intervals of interest.
     channel_name: list: List with channel names of interest.
-
+    study: int: Study number: 1 = block design, 2 = trial design
     Return:
     ------
     csv file with power per trial per frequency band
@@ -74,23 +76,31 @@ def save_band_power_per_trial(time_intervals={"pre": (-0.2, 0)}, channel_name=["
     # save single subject dataframes in a list
     brain_behav = []
 
+    if study == 1:
+        id_list = id_list_expecon1
+    elif study == 2:
+        id_list = id_list_expecon2
+    
     # loop over subjects
     for subj in id_list:
+        if study == 2:
+            if subj == "013":
+                continue
         # load single trial power
-        power = mne.time_frequency.read_tfrs(tfr_dir / f"{subj}_single_trial_power-tfr.h5")[0]
+        power = mne.time_frequency.read_tfrs(tfr_dir / f"{subj}_single_trial_power_{str(study)}-tfr.h5")[0]
 
         # get behavioral data
         behav_data = power.metadata
 
         # loop over the time intervals
         for keys, time in time_intervals.items():
+
             power_crop = power.copy().crop(tmin=time[0], tmax=time[1]).pick_channels(channel_name)
 
             # now we average over time and channels
             power_crop.data = np.mean(power_crop.data, axis=(1, 3))
 
-            # now we average over the frequency band and time interval
-            # and add the column to the behavioral data frame
+            # now we average over the frequency band
             behav_data[f"{keys}_alpha"] = np.mean(power_crop.data[:, 4:11], axis=1)  # 7-13 Hz
             behav_data[f"{keys}_beta"] = np.mean(power_crop.data[:, 12:23], axis=1)  # 15-25 Hz
 
@@ -98,18 +108,16 @@ def save_band_power_per_trial(time_intervals={"pre": (-0.2, 0)}, channel_name=["
         brain_behav.append(behav_data)
 
     # concatenate the list of dataframes and save as csv
-    pd.concat(brain_behav).to_csv(behav_dir / "brain_behav.csv")
+    pd.concat(brain_behav).to_csv(behav_dir / "brain_behav_{study}.csv")
 
     return brain_behav
 
 
-def power_criterion_corr():
+def power_criterion_corr() -> None:
     """
-    Correlate the power difference between low and high expectations trials.
-
-    Difference is measured in dprime and criterion for different frequency bands.
-    TODO(simon): improve slightly
-    Plot a regression plot.
+    Correlate the power difference between low and high expectations trials 
+    with the difference in sdt parameters for low and high expectations.
+    function calculates spearman correlation and plots regression line.
     """
     # load brain behav dataframe
     df_brain_behav = pd.read_csv(behav_dir / "brain_behav.csv")
@@ -120,7 +128,8 @@ def power_criterion_corr():
 
     # now get power per participant and per condition
     for f in freqs:
-        power = df_brain_behav.groupby(["ID", "cue"]).mean()[f]
+
+        power = df_brain_behav.groupby(["ID", "cue"])[f].mean()
 
         low = power.unstack()[0.25].reset_index()
         high = power.unstack()[0.75].reset_index()
@@ -133,34 +142,33 @@ def power_criterion_corr():
     # add the power difference to a dictionary
     power_dict = {"alpha": diff_p_list[0], "beta": diff_p_list[1]}
 
-    # load random effects
-    # re = pd.read_csv(Path("./data/behav/mixed_models/brms/random_effects.csv"))  # TODO(simon): rm if not needed
-
     # load behavioral SDT data
-    out = figure1.prepare_for_plotting()
+    out = figure1.prepare_for_plotting(exclude_high_fa = False, expecon = 1)
 
+    # get sdt parameters
     sdt = out[0][0]
-    # calculate criterion difference
+
+    # calculate criterion difference between conditions
     c_diff = np.array(sdt.criterion[sdt.cue == 0.25]) - np.array(sdt.criterion[sdt.cue == 0.75])
+
     # dprime difference between conditions
     dprime_diff = np.array(sdt.dprime[sdt.cue == 0.25]) - np.array(sdt.dprime[sdt.cue == 0.75])
 
     # add the sdt data to a dictionary
     sdt_params = {"dprime": dprime_diff, "criterion": c_diff}
 
-    for p_key, p_value in power_dict.items():  # loop over the power difference
-        for keys, values in sdt_params.items():  # loop over the criterion and dprime difference
+    for freq_band, power_values in power_dict.items():  # loop over the power difference
+        for sdt_param, sdt_values in sdt_params.items():  # loop over the criterion and dprime difference
             # plot regression line
-            fig = sns.regplot(x=p_value, y=values)
+            fig = sns.regplot(x=power_values, y=sdt_values)
 
-            plt.xlabel(f"{p_key} power difference")
-            plt.ylabel(f"{keys} difference")
+            plt.xlabel(f"{freq_band} power difference")
+            plt.ylabel(f"{sdt_param} difference")
             plt.show()
-            # calculate correlation
-            print(scipy.stats.spearmanr(p_value, values))
+            # calculate correlation (spearman is non parametric)
+            print(scipy.stats.spearmanr(power_values, sdt_values))
             # save figure
-            fig.figure.savefig(Path(path_to.figures.manuscript.figure7, f"{p_key}_{keys}_.svg"))
-            # TODO(simon): do you want the final '_' here?
+            fig.figure.savefig(Path(path_to.figures.manuscript.figure3, f"{freq_band}_{sdt_param}.svg"))
             plt.show()
 
 
