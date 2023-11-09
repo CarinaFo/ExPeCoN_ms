723c68c6298dfc7336e57bf3f677f8876438a27e
CarinaFo <carinaforster0611@gmail.com>
2023-11-09 11:30:37 +0100

    oct6 file as source space (5mm spacing)

--- code/expecon_ms/eeg/source/source_reco.py
+++ code/expecon_ms/eeg/source/source_reco.py
@@ -47,9 +47,20 @@ subjects_dir = fetch_fsaverage()
 # set root path to fsaverag files
 fs_average_root_path = f'{subjects_dir}{Path("/")}bem{Path("/")}'
 
+create_new_source_space = False
+
+# create source space
+if create_new_source_space:
+
+    src = mne.setup_source_space(
+        'fsaverage', spacing="oct6", add_dist="patch", subjects_dir=subjects_dir
+    )
+
+    mne.write_source_spaces(f'{fs_average_root_path}fsaverage-oct6-src.fif', src) 
+
 # load bem solution, source space and transformation matrix
 bem = f'{fs_average_root_path}fsaverage-5120-5120-5120-bem-sol.fif'
-src_fname = f'{fs_average_root_path}fsaverage-ico-5-src.fif'
+src_fname = f'{fs_average_root_path}fsaverage-oct6-src.fif' # 5mm between sources
 trans_dir = f'{fs_average_root_path}fsaverage-trans.fif'
 
 # Read the source space
@@ -64,7 +75,7 @@ epochs = mne.read_epochs(Path(dir_clean_epochs / f"P015_icacorr_0.1Hz-epo.fif"))
 
 # set up forward solution
 fwd = mne.make_forward_solution(
-    epochs.info, trans=trans_dir, src=src, bem=bem, eeg=True, 
+    epochs.info, trans=trans_dir, src=src, bem=bem, eeg=True,
     mindist=5.0,
     n_jobs=None
 )
@@ -96,6 +107,17 @@ hit_fa_diff = config.behavioral_cleaning.hit_fa_diff
 # pilot data counter for expecon 1
 pilot_counter = config.participants.pilot_counter
 # %% Functions >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
+
+# run source reco function:
+#run_source_reco(study=1, cond="probability", dics=True, fmin=15, fmax=25, 
+#                tmin=-0.4, tmax=0, save_path = beamformer_path, 
+#                drop_bads = True, plot_alignment=False)
+
+# load source array
+#stc_array = create_source_contrast_array(study=1, 
+ #                                        cond_a='high', 
+  #                                       cond_b='low', 
+   #                                      path_to_source=beamformer_path)
 
 
 def run_source_reco(study: int, 
@@ -153,152 +175,162 @@ def run_source_reco(study: int,
         # print participant ID
         print("Analyzing " + subj)
 
-        # load clean epochs (after ica component rejection)
-        if study == 1:
-            epochs = mne.read_epochs(
-                Path(dir_clean_epochs / f"P{subj}_icacorr_0.1Hz-epo.fif"))
-        elif study == 2:
-            # skip ID 13
-            if subj == '013':
-                continue
-            epochs = mne.read_epochs(dir_clean_epochs_expecon2 / f"P{subj}_icacorr_0.1Hz-epo.fif")
-            # rename columns
-            epochs.metadata = epochs.metadata.rename(columns ={"resp1_t": "respt1",
-                                                               "stim_type": "isyes",
-                                                               "resp1": "sayyes"})
-        else:
-            raise ValueError("input should be 1 or 2 for the respective study")
-
-        # clean epochs (remove blocks based on hit and false alarm rates, reaction times, etc.)
-        epochs = drop_trials(data=epochs)
-
-        # get behavioral data for current participant
-        if study == 1:
-            subj_data = data[idx + pilot_counter == data.ID]
-        elif study == 2:
-            subj_data = data[idx + 1 == data.ID]
-        else:
-            raise ValueError("input should be 1 or 2 for the respective study")
-
-        # get drop log from epochs
-        drop_log = epochs.drop_log
-
-        # Ignored bad epochs are those defined by the user as bad epochs
-        search_string = "IGNORED"
-        # remove trials where epochs are labelled as too short
-        indices = [index for index, tpl in enumerate(drop_log) if tpl and search_string not in tpl]
-
-        # drop trials without corresponding epoch
-        if indices:
-            epochs.metadata = subj_data.reset_index().drop(indices)
-        else:
-            epochs.metadata = subj_data
-
-        if drop_bads:
-            # drop epochs with abnormal strong signal (> 200 micro-volts)
-            epochs.drop_bad(reject=dict(eeg=200e-6))
-
         if cond == "probability":
-            epochs_a = epochs[
-                (epochs.metadata.cue == 0.75)
-            ]
-            epochs_b = epochs[(epochs.metadata.cue == 0.25)]
             cond_a_name = "high"
             cond_b_name = "low"
+            source_files = Path(save_path, f"{cond_a_name}_{subj}_{study}-lh.stc")
         elif cond == "prev_resp":
-            epochs_a = epochs[((epochs.metadata.prevresp == 1) &
-                                (epochs.metadata.cue == 0.75))]
-            epochs_b = epochs[((epochs.metadata.prevresp == 0) &
-                                (epochs.metadata.cue == 0.75))]
-            cond_a_name = "prevyesresp_highprob"
-            cond_b_name = "prevnoresp_highprob"
+            source_files = Path(save_path, f"contrast_{cond}_{subj}_{study}-lh.stc")
+
+        if source_files.exists():
+            print(f"Skipping {subj} because it already exists.")
+            continue
         else:
-            raise ValueError("input should be 'probability' or 'prev_resp'")
+            print(f"Processing {subj}.")
+            # load clean epochs (after ica component rejection)
+            if study == 1:
+                epochs = mne.read_epochs(
+                    Path(dir_clean_epochs / f"P{subj}_icacorr_0.1Hz-epo.fif"))
+            elif study == 2:
+                # skip ID 13
+                if subj == '013':
+                    continue
+                epochs = mne.read_epochs(dir_clean_epochs_expecon2 / f"P{subj}_icacorr_0.1Hz-epo.fif")
+                # rename columns
+                epochs.metadata = epochs.metadata.rename(columns ={"resp1_t": "respt1",
+                                                                "stim_type": "isyes",
+                                                                "resp1": "sayyes"})
+            else:
+                raise ValueError("input should be 1 or 2 for the respective study")
 
-        # make sure we have an equal amount of trials in both conditions
-        mne.epochs.equalize_epoch_counts([epochs_a, epochs_b])
+            # clean epochs (remove blocks based on hit and false alarm rates, reaction times, etc.)
+            epochs = drop_trials(data=epochs)
 
-        if dics == 1:
-            
-            # save path for source estimates
-            save_path = beamformer_path
+            # get behavioral data for current participant
+            if study == 1:
+                subj_data = data[idx + pilot_counter == data.ID]
+            elif study == 2:
+                subj_data = data[idx + 1 == data.ID]
+            else:
+                raise ValueError("input should be 1 or 2 for the respective study")
 
-            # We are interested in the beta band
-            freqs = np.arange(fmin, fmax, 1)
+            # get drop log from epochs
+            drop_log = epochs.drop_log
 
-            # Computing the cross-spectral density matrix for the beta frequency band, for
-            # different time intervals.
-            # csd for all epochs
-            csd = mne.time_frequency.csd_morlet(epochs, freqs, tmin=tmin, tmax=tmax)
-            # csd for high-prob trials only
-            csd_a = mne.time_frequency.csd_morlet(epochs_a, freqs, tmin=tmin, tmax=tmax)
-            # csd for low-prob trials only
-            csd_b = mne.time_frequency.csd_morlet(epochs_b, freqs, tmin=tmin, tmax=tmax)
+            # Ignored bad epochs are those defined by the user as bad epochs
+            search_string = "IGNORED"
+            # remove trials where epochs are labelled as too short
+            indices = [index for index, tpl in enumerate(drop_log) if tpl and search_string not in tpl]
 
-            info = epochs.info
+            # drop trials without corresponding epoch
+            if indices:
+                epochs.metadata = subj_data.reset_index().drop(indices)
+            else:
+                epochs.metadata = subj_data
 
-            # To compute the source power for a frequency band, rather than each frequency
-            # separately, we average the CSD objects across frequencies.
-            csd_a = csd_a.mean()
-            csd_b = csd_b.mean()
+            if drop_bads:
+                # drop epochs with abnormal strong signal (> 200 micro-volts)
+                epochs.drop_bad(reject=dict(eeg=200e-6))
 
-            # Computing DICS spatial filters using the CSD that was computed for all epochs
-            filters = mne.beamformer.make_dics(
-                info, fwd, csd, noise_csd=None, 
-                pick_ori="max-power", reduce_rank=True, real_filter=True
-            )
+            if cond == "probability":
+                epochs_a = epochs[
+                    (epochs.metadata.cue == 0.75)
+                ]
+                epochs_b = epochs[(epochs.metadata.cue == 0.25)]
+            elif cond == "prev_resp":
+                epochs_a = epochs[((epochs.metadata.prevresp == 1) &
+                                    (epochs.metadata.cue == 0.75))]
+                epochs_b = epochs[((epochs.metadata.prevresp == 0) &
+                                    (epochs.metadata.cue == 0.75))]
+                cond_a_name = "prevyesresp_highprob"
+                cond_b_name = "prevnoresp_highprob"
+            else:
+                raise ValueError("input should be 'probability' or 'prev_resp'")
 
-            # Applying DICS spatial filters separately to each condition
-            source_power_a, freqs = mne.beamformer.apply_dics_csd(csd_a, filters)
-            source_power_b, freqs = mne.beamformer.apply_dics_csd(csd_b, filters)
+            # make sure we have an equal amount of trials in both conditions
+            mne.epochs.equalize_epoch_counts([epochs_a, epochs_b])
 
-            source_power_a.save(Path(save_path, f"{cond_a_name}_{subj}_{study}"))
-            source_power_b.save(Path(save_path, f"{cond_b_name}_{subj}_{study}"))
+            if dics == 1:
+                
+                # save path for source estimates
+                save_path = beamformer_path
 
-        else:
+                # We are interested in the beta band
+                freqs = np.arange(fmin, fmax, 1)
 
-            save_path = mne_path
+                # Computing the cross-spectral density matrix for the beta frequency band, for
+                # different time intervals.
+                # csd for all epochs
+                csd = mne.time_frequency.csd_morlet(epochs, freqs, tmin=tmin, tmax=tmax)
+                # csd for high-prob trials only
+                csd_a = mne.time_frequency.csd_morlet(epochs_a, freqs, tmin=tmin, tmax=tmax)
+                # csd for low-prob trials only
+                csd_b = mne.time_frequency.csd_morlet(epochs_b, freqs, tmin=tmin, tmax=tmax)
 
-            # average epochs for MNE
-            evokeds_a = epochs_a.average()
-            evokeds_b = epochs_b.average()
+                info = epochs.info
 
-            evoked_contrast = mne.combine_evoked(all_evoked=[evokeds_a, evokeds_b], 
-                                                 weights=[0.5, -0.5])
-            evoked_contrast.crop(tmin, tmax)
-            # filter in beta band
-            evoked_contrast.filter(l_freq=fmin, h_freq=fmax)
+                # To compute the source power for a frequency band, rather than each frequency
+                # separately, we average the CSD objects across frequencies.
+                csd_a = csd_a.mean()
+                csd_b = csd_b.mean()
 
-            # create noise covariance with a bias of data length
-            # noise_cov = create_noise_cov(evokeds_high.data.shape, evokeds_high.info)
+                # Computing DICS spatial filters using the CSD that was computed for all epochs
+                filters = mne.beamformer.make_dics(
+                    info, fwd, csd, noise_csd=None, 
+                    pick_ori="max-power", reduce_rank=True, real_filter=True
+                )
 
-            # all epochs for noise covariance computation
-            noise_cov = mne.compute_covariance(epochs, tmin=tmin, tmax=tmax,
-                                                method=["shrunk", "empirical"],
-                                                  rank="info")
+                # Applying DICS spatial filters separately to each condition
+                source_power_a, freqs = mne.beamformer.apply_dics_csd(csd_a, filters)
+                source_power_b, freqs = mne.beamformer.apply_dics_csd(csd_b, filters)
 
-            # save covariance matrix
-            mne.write_cov(fname="covariance_prestim.cov", cov=noise_cov)
+                source_power_a.save(Path(save_path, f"{cond_a_name}_{subj}_{study}"))
+                source_power_b.save(Path(save_path, f"{cond_b_name}_{subj}_{study}"))
 
-            # fixed forward solution for MNE methods
-            fwd_fixed = mne.convert_forward_solution(fwd, surf_ori=True)
+            else:
 
-            info = evoked_contrast.info
+                save_path = mne_path
 
-            inv_op = mne.minimum_norm.make_inverse_operator(info, fwd_fixed, 
-                                                            noise_cov, loose=0.2, 
-                                                            depth=0.8)
+                # average epochs for MNE
+                evokeds_a = epochs_a.average()
+                evokeds_b = epochs_b.average()
 
-            evoked_contrast.set_eeg_reference(projection=True)  # needed for inverse modeling
+                evoked_contrast = mne.combine_evoked(all_evoked=[evokeds_a, evokeds_b], 
+                                                    weights=[0.5, -0.5])
+                evoked_contrast.crop(tmin, tmax)
+                # filter in beta band
+                evoked_contrast.filter(l_freq=fmin, h_freq=fmax)
 
-            method = "dSPM"
-            snr = 3.0
-            lambda2 = 1.0 / snr**2  # regularization
+                # create noise covariance with a bias of data length
+                # noise_cov = create_noise_cov(evokeds_high.data.shape, evokeds_high.info)
 
-            stc = mne.minimum_norm.apply_inverse(evoked_contrast, inv_op, lambda2, 
-                                                 method=method, pick_ori=None)
+                # all epochs for noise covariance computation
+                noise_cov = mne.compute_covariance(epochs, tmin=tmin, tmax=tmax,
+                                                    method=["shrunk", "empirical"],
+                                                    rank="info")
 
-            stc.save(Path(save_path, f"contrast_{cond}_{subj}_{study}"))
+                # save covariance matrix
+                mne.write_cov(fname="covariance_prestim.cov", cov=noise_cov)
+
+                # fixed forward solution for MNE methods
+                fwd_fixed = mne.convert_forward_solution(fwd, surf_ori=True)
+
+                info = evoked_contrast.info
+
+                inv_op = mne.minimum_norm.make_inverse_operator(info, fwd_fixed, 
+                                                                noise_cov, loose=0.2, 
+                                                                depth=0.8)
+
+                evoked_contrast.set_eeg_reference(projection=True)  # needed for inverse modeling
+
+                method = "dSPM"
+                snr = 3.0
+                lambda2 = 1.0 / snr**2  # regularization
+
+                stc = mne.minimum_norm.apply_inverse(evoked_contrast, inv_op, lambda2, 
+                                                    method=method, pick_ori=None)
+
+                stc.save(Path(save_path, f"contrast_{cond}_{subj}_{study}"))
 
 
 def create_source_contrast_array(study: int,
@@ -406,8 +438,9 @@ def spatio_temporal_source_test(
 
     # plot average source or t values
     brain = stc.plot(
-        hemi=hemisphere, views=view, subjects_dir=subjects_dir, subject="fsaverage",
-         time_viewer=True, background="white", colorbar=False
+        hemi=hemisphere, views=view, subjects_dir=subjects_dir, 
+        subject="fsaverage",
+        time_viewer=True, background="white", colorbar=False
     )
 
     brain.save_image(f'{save_path_source_figs}{Path("/")}grand_average_{cond}_{method}_{study}_' +
@@ -491,6 +524,7 @@ def create_noise_cov(data_size: tuple[int, int], data_info: mne.Info) -> mne.Cov
     return mne.compute_raw_covariance(raw1, tmin=0, tmax=None)
 
 # Helper functions
+
 
 def plot_source_space_electrodes_alignment():
 
