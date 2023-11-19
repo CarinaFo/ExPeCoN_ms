2cb920c3ed11258d6e6d51764f8c33b40972cd68
CarinaFo <carinaforster0611@gmail.com>
2023-11-19 11:43:03 +0100

    cleaned up tfr script and added func lines

--- code/expecon_ms/eeg/sensor/tfr_trial.py
+++ code/expecon_ms/eeg/sensor/tfr_trial.py
@@ -21,6 +21,7 @@ import seaborn as sns
 
 from expecon_ms.behav import figure1
 from expecon_ms.configs import PROJECT_ROOT, config, path_to
+
 # %% Set global vars & paths >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 # Specify the file path for which you want the last commit date
@@ -50,10 +51,11 @@ freq_bands = {"alpha": (7, 13), "beta": (15, 25)}
 
 # %% Functions >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
+# add this line to jupyter script
+# save_band_power_per_trial(study=2, time_intervals={'pre': [(-0.2,0), (-0.3, -0.1)]}, channel_name=['CP4'])
 
-def save_band_power_per_trial(time_intervals: dict,
-                              channel_name: list,
-                              study: int):
+def save_band_power_per_trial(study: int, time_intervals: dict,
+                              channel_name: list):
     """
     Save the power per trial per frequency band in a csv file.
 
@@ -65,10 +67,11 @@ def save_band_power_per_trial(time_intervals: dict,
 
     Args:
     ----
+    study: int: Study number: 1 = block design, 2 = trial design
     time_intervals: dict: Dictionary with column names as keys and values
         as tuples with the time interval of interest.
     channel_name: list: List with channel names of interest.
-    study: int: Study number: 1 = block design, 2 = trial design
+
     Return:
     ------
     csv file with power per trial per frequency band
@@ -80,29 +83,38 @@ def save_band_power_per_trial(time_intervals: dict,
         id_list = id_list_expecon1
     elif study == 2:
         id_list = id_list_expecon2
-    
+
     # loop over subjects
     for subj in id_list:
-        if study == 2:
-            if subj == "013":
-                continue
+        if ((study == 2) & (subj == "013")):
+            continue
         # load single trial power
         power = mne.time_frequency.read_tfrs(tfr_dir / f"{subj}_single_trial_power_{str(study)}-tfr.h5")[0]
 
         # get behavioral data
         behav_data = power.metadata
 
-        # loop over the time intervals
-        for keys, time in time_intervals.items():
+        # at which time window do we want to look at?
+        if study == 1:
+            power_crop = power.copy().crop(tmin=time_intervals['pre'][0][0],
+                 tmax=time_intervals['pre'][0][1]).pick_channels(channel_name)
+        else:
+            power_crop = power.copy().crop(tmin=time_intervals['pre'][1][0],
+                 tmax=time_intervals['pre'][1][1]).pick_channels(channel_name)
 
-            power_crop = power.copy().crop(tmin=time[0], tmax=time[1]).pick_channels(channel_name)
+        # now we average over time and channels
+        power_crop.data = np.mean(power_crop.data, axis=(1, 3))
+        # using list comprehension + enumerate()
 
-            # now we average over time and channels
-            power_crop.data = np.mean(power_crop.data, axis=(1, 3))
+        # extract the indices of the frequency bands
+        alpha_indices = [idx for idx, val in enumerate(freq_list) if val >= freq_bands['alpha'][0]
+                            and val <= freq_bands['alpha'][1]]
+        beta_indices = [idx for idx, val in enumerate(freq_list) if val >= freq_bands['beta'][0]
+                        and val <= freq_bands['beta'][1]]
 
-            # now we average over the frequency band
-            behav_data[f"{keys}_alpha"] = np.mean(power_crop.data[:, 4:11], axis=1)  # 7-13 Hz
-            behav_data[f"{keys}_beta"] = np.mean(power_crop.data[:, 12:23], axis=1)  # 15-25 Hz
+        # now we average over the frequency band
+        behav_data[f"pre_alpha"] = np.mean(power_crop.data[:, alpha_indices], axis=1)  # 7-13 Hz
+        behav_data[f"pre_beta"] = np.mean(power_crop.data[:, beta_indices], axis=1)  # 15-25 Hz
 
         # save the data in a list
         brain_behav.append(behav_data)
@@ -114,10 +126,17 @@ def save_band_power_per_trial(time_intervals: dict,
 
 
 def power_criterion_corr(study: int) -> None:
-    """
-    Correlate the power difference between low and high expectations trials 
-    with the difference in sdt parameters for low and high expectations.
-    function calculates spearman correlation and plots regression line.
+    """Correlate power vs. criterion difference.
+
+    Plots regression line and calculates spearman correlation.
+
+    Args:
+    ----
+    study: int: Study number: 1 = block design, 2 = trial design
+
+    Return:
+    ------
+    None
     """
     # load brain behav dataframe
     df_brain_behav = pd.read_csv(behav_dir / f"brain_behav_{study}.csv")
